565b0bc186a8d2711968e982ebf5c95e18df4ec9
Anna <annaalexandracool@gmail.com>
2025-01-19 16:10:41 -0800

    Update

---
+++ main.js
@@ -0,0 +1,5 @@
+import "./style.css"
+
+document.querySelector("#app").innerHTML = `
+  <button class="enableEthereumButton">Enable Ethereum</button>
+  <h2>Account: <span class="showAccount"></span></h2>`
